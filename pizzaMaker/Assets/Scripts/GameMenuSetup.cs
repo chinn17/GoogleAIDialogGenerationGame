15090bd03167b8383c7b6f2e90a776d0b8e6b9a0
Daniel Mossaband <Daniel@Mossaband.com>
2019-04-14 23:53:04 -0700

    Added more functions to change online status. Created transitions. Overloaded some functions

---
+++ pizzaMaker/Assets/Scripts/GameMenuSetup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameMenuSetup : MonoBehaviour
+{
+
+    GameObject waitingPopup;
+    GameObject joinPopup;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        waitingPopup = GameObject.Find("Waiting Popup");
+        joinPopup = GameObject.Find("Join Popup");
+    }
+
+    void Start()
+    {
+        SetWaitingPopup(false);
+        SetJoinPopup(false);
+    }
+
+    public void SetWaitingPopup(bool givenBool)
+    {
+        waitingPopup.SetActive(givenBool);
+    }
+
+    public void SetJoinPopup(bool givenBool)
+    {
+        joinPopup.SetActive(givenBool);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
