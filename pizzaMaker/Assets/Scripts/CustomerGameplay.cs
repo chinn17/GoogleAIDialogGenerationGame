04faf6de3f7bf05882afd73b0d7da8ba67394d1e
U Khyoi Nu <khyo@Us-MacBook-Pro.local>
2019-04-17 20:55:45 -0700

    Add score and action to pay button

---
+++ pizzaMaker/Assets/Scripts/CustomerGameplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CustomerGameplay : MonoBehaviour
+{
+    TextMeshProUGUI scoreText;
+    TextMeshProUGUI totalText;
+    string score;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        score = "0";
+    }
+
+    private void Start()
+    {
+    }
+
+    public void GetTotal()
+    {
+        totalText = GetComponent<TextMeshProUGUI>();
+        score = totalText.text;
+        Debug.Log(score);
+    }
+
+    public void UpdateScore()
+    {
+        //scoreText.text = "Score : " + score;
+        //Debug.Log(score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
