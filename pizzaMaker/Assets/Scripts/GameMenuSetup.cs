24d8a08e4a0367aad31fab6799c55828ebf88062
Daniel Mossaband <Daniel@Mossaband.com>
2019-05-02 01:14:52 -0700

    Added working register feature to game using sockets

--- pizzaMaker/Assets/Scripts/GameMenuSetup.cs
+++ pizzaMaker/Assets/Scripts/GameMenuSetup.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using TMPro;
 using UnityEngine;
 
@@ -8,7 +11,6 @@ public class GameMenuSetup : MonoBehaviour
 
     GameObject waitingPopup;
     GameObject joinPopup;
-
     // Start is called before the first frame update
     void Awake()
     {
@@ -32,8 +34,38 @@ public class GameMenuSetup : MonoBehaviour
         joinPopup.SetActive(givenBool);
     }
     // Update is called once per frame
+
+    public void ConnectToServer()
+    {
+        string toSend = "register/%username=test%password=test%email=test@test.com%";
+
+        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4343);
+
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.Connect(serverAddress);
+
+        // Sending
+        int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+        byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+        byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+        clientSocket.Send(toSendLenBytes);
+        clientSocket.Send(toSendBytes);
+
+        // Receiving
+        byte[] rcvLenBytes = new byte[4];
+        clientSocket.Receive(rcvLenBytes);
+        int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+        byte[] rcvBytes = new byte[rcvLen];
+        clientSocket.Receive(rcvBytes);
+        string rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+        Debug.Log("Client received: " + rcv);
+
+        clientSocket.Close();
+    }
+
     void Update()
     {
-        
+
     }
 }
