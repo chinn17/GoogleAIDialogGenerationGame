e909532cc3d8c0fe5f930d3a505585cbeec941d8
U Khyoi Nu <khyo@Us-MacBook-Pro.local>
2019-05-06 16:08:04 -0700

    Update menu and cart UI with switching option

---
+++ pizzaMaker/Assets/Scripts/GameplaySetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplaySetup : MonoBehaviour
+{
+    GameObject cart;
+    GameObject menu;
+
+    bool status = true;
+
+    void Awake()
+    {
+        cart = GameObject.Find("Cart");
+        menu = GameObject.Find("Menu");
+    }
+
+    void Start()
+    {
+        menu.SetActive(status);
+        cart.SetActive(!status);
+    }
+
+    public void FlipStatus()
+    {
+        status = !status;
+        Debug.Log(status);
+        menu.SetActive(status);
+        cart.SetActive(!status);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
